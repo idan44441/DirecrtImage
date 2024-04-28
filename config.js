6594743483757c2b334d73ee11acb315464f08e2
idan44441 <95760294+idan44441@users.noreply.github.com>
2024-04-28 17:40:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,40 @@
 const mysql = require("mysql2");
-const con = mysql.createConnection({
+
+// Create a pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // adjust as per your requirement
     host: 'sql.freedb.tech',
     user: 'freedb_idan44',
     password: 'fPecEFr5!hvhuM3',
     database:'freedb_IdanDB',
     port: '3306'
-
 });
 
+// Get connection from the pool
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to database: ', err);
+        return;
+    }
 
+    console.log('Connected to database');
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+    // Use the connection for querying
 
-module.exports = con;
+    // Example query
+    connection.query('SELECT * FROM USER', (error, results, fields) => {
+        if (error) {
+            console.error('Error executing query: ', error);
+            connection.release(); // Release the connection back to the pool
+            return;
+        }
+
+        // Do something with the results
+
+        // Release the connection back to the pool
+        connection.release();
+    });
+});
+
+// Export the pool
+module.exports = pool;
